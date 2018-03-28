8e26246da5effffcc016ba78c56385278057b556
Benjamin Standfield <benjaminstandfield@gmail.com>
2018-03-27 22:29:21 -0400

    added functionality for charging station status

--- index.js
+++ index.js
@@ -45,10 +45,54 @@ server.get("/", function(req,res){
 server.post("/", function(req,res){
   req.body.StationID;
   if (req.body.StationID==1){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation1.PowerDraw;
     ChargingStation1.PowerDraw=req.body.PowerDraw;
-    currentPower = currentPower - req.body.PowerDraw;
     ChargingStation1.inuse=req.body.inuse;
   }
+  if(req.body.StationID==2){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation2.PowerDraw;
+    ChargingStation2.PowerDraw=req.body.PowerDraw;
+    ChargingStation2.inuse=req.body.inuse;
+  }
+  if(req.body.StationID==3){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation3.PowerDraw;
+    ChargingStation3.PowerDraw=req.body.PowerDraw;
+    ChargingStation3.inuse=req.body.inuse;
+  }
+  if(req.body.StationID==4){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation4.PowerDraw;
+    ChargingStation4.PowerDraw=req.body.PowerDraw;
+    ChargingStation4.inuse=req.body.inuse;
+  }
+  if(req.body.StationID==5){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation5.PowerDraw;
+    ChargingStation5.PowerDraw=req.body.PowerDraw;
+    ChargingStation5.inuse=req.body.inuse;
+  }
+  if(req.body.StationID==6){
+    if(req.body.PowerDraw > 0)
+      currentPower = currentPower - req.body.PowerDraw;
+    else
+      currentPower = currentPower + ChargingStation6.PowerDraw;
+    ChargingStation6.PowerDraw=req.body.PowerDraw;
+    ChargingStation6.inuse=req.body.inuse;
+  }
+
   res.end(JSON.stringify({}))
 
 })
